c8acaa0862d4e207cb6c67d06ff1296a7d2d4360
ericmuh <muhwezi1000@gmail.com>
2021-04-14 12:46:48 +0300

    finish todo

--- scripts.js
+++ scripts.js
@@ -1,41 +1,54 @@
 // Referenciing all the necessary Elements.
 
-let ourInput = document.querySelector(".our-input")
-let ourBtn = document.querySelector(".our-btn")
-let ourTodoList = document.querySelector(".our-todolist")
+let ourInput = document.querySelector(".our-input");
+let ourBtn = document.querySelector(".our-btn");
+let ourTodoList = document.querySelector(".our-todolist");
 
 // console.log(ourInput, ourBtn, ourTodoList);
 
-function AddTodo(){
+function AddTodo() {
+  if (ourInput.value.length > 0) {
     console.log(ourInput.value);
     // Creating our 'li' dynamically using Javascript.
-    let ourLi = document.createElement("li")
+    let ourLi = document.createElement("li");
     // set the innetHTML to the input value.
 
     // concatenating the value to an html button
-     // ourLi.innerHTML = ourInput.value+"<button> delete</button>"
-    ourLi.innerHTML = `${ourInput.value}<button class='del-btn'> x </button>`
+    // ourLi.innerHTML = ourInput.value+"<button> delete</button>"
+    ourLi.innerHTML = `${ourInput.value}<button class='del-btn'> x </button>`;
 
     // This adds the li from line 13 to the ol which is in our html
-    ourTodoList.appendChild(ourLi)
+    ourTodoList.appendChild(ourLi);
 
     ourInput.value = "";
-    RemoveTodo()
-
-
+    RemoveTodo();
+    FinishTodo()
+  } else {
+    alert("Please type something");
+  }
 }
 
-ourBtn.addEventListener("click", AddTodo)
-
+ourBtn.addEventListener("click", AddTodo);
 
 // Remove Todo
-function RemoveTodo(){
-    let removeBtns = document.querySelectorAll('.del-btn')
+function RemoveTodo() {
+  let removeBtns = document.querySelectorAll(".del-btn");
 
-    removeBtns.forEach(function(button){
-        button.addEventListener('click', function(){
-            this.parentElement.remove()
+  removeBtns.forEach(function (button) {
+    button.addEventListener("click", function () {
+      this.parentElement.remove();
+    });
+  });
+}
+
+// Finish Todo
+function FinishTodo(){
+    let ourtodos = document.querySelectorAll("li")
+    ourtodos.forEach(function(li){
+        li.addEventListener("click",function(){
+          li.classList.toggle("finished")
+          
         })
-
     })
+
 }
